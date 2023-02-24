7f1baccd9b0ae0c750bb5f546b3f8991cce396c6
Russell Oyama <russelloyama@gmail.com>
2023-02-24 09:55:37 -1000

    setting up mongodb/mongoose in project and creating default items in db

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
-
-const items = ["buy food", "cook food", "eat food"];
-const workItems = []
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemsSchema = {
+    name: String
+};
+
+const Item = new  mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome to your todo list!"
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+    name: "<--- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function(err){
+    if(err){
+        console.log(err);
+    } else {
+        console.log("Successfully saved default items to database!");
+    }
+});
+
 app.get("/", function(req, res){
     
-    const day = date.getDate();
-    
-    res.render('list', {listTitle: day, newListItems: items})
+    res.render('list', {listTitle: "Today", newListItems: items})
 });
 
 
