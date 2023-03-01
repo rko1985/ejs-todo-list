3431a8d914635ac2ea7fee05d0cd1a8500e94221
Russell Oyama <russelloyama@gmail.com>
2023-03-01 07:50:28 -1000

    deleting items

--- app.js
+++ app.js
@@ -70,6 +70,19 @@ app.post('/', function(req, res){
 
 });
 
+app.post('/delete', function(req, res){
+    const checkedItemId = req.body.checkbox;
+
+    Item.findByIdAndRemove(checkedItemId, function(err){
+        if(err){
+            console.log(err);
+        } else {
+            console.log("Successfully deleted item!");
+            res.redirect("/");
+        }
+    });
+});
+
 app.get("/work", function(req,res){
     res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
